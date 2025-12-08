2bcaf0ae43485c410ad5775619431152b9d29833
Hhamed91 <hazem.hamed.h@hotmail.com>
2025-12-08 16:24:14 -0500

    Initial personal site

--- personal-site/app/blog/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function BlogPage() {
-  return <div className="p-8 text-2xl">Blog posts coming soon...</div>;
-}
--- personal-site/app/projects/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function BlogPage() {
-  return <div className="p-8 text-2xl">Projects coming soon...</div>;
-}
